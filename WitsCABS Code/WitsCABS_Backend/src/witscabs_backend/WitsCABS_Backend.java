5a697ad74aede99dc90fae690790641b02b77fc8
Jason Stuart <1171733@students.wits.ac.za>
2017-09-07 13:42:09 +0200

    Implemented Connection To MySQL Database On local machine

--- WitsCABS Code/WitsCABS_Backend/src/witscabs_backend/WitsCABS_Backend.java
+++ WitsCABS Code/WitsCABS_Backend/src/witscabs_backend/WitsCABS_Backend.java
@@ -5,6 +5,14 @@
  */
 package witscabs_backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Jason
@@ -14,8 +22,29 @@ public class WitsCABS_Backend {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        String url = "jdbc:mysql://localhost:3306/WitsCABS?useSSL=false";
+        String username = "root";
+        String password = "jason";
+
+        System.out.println("Connecting database...");
+        try 
+        {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement s = connection.createStatement();
+            ResultSet rs = s.executeQuery("SELECT * FROM Driver;");
+            while(rs.next())
+            {
+                System.out.println(rs.getString("Last_Name"));
+            }
+            
+            connection.close();
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println("Failed to connect to Database. Reason:" + ex);
+        }
+        
     }
     
 }
