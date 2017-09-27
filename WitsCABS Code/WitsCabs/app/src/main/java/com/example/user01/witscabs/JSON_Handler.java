51e7d37196bea9fee9e9572b9268d10498c572ce
Amine Boukrout <886515@students.wits.ac.za>
2017-09-27 15:48:19 +0200

    networking not working yet

---
+++ WitsCABS Code/WitsCabs/app/src/main/java/com/example/user01/witscabs/JSON_Handler.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.user01.witscabs;
+
+/**
+ *
+ * @author Jason
+ */
+
+//makes sure server can handle JSON files (its basic and my code)
+public class JSON_Handler
+{
+    //store variable type, name and actual value.
+    private String[] varName;
+    private String[] varType;
+    private String[] value;
+
+    //when string comes in, split it correctly for storage
+    public JSON_Handler(String input)
+    {
+        input = input.replace("{", "");
+        input = input.replace("}", "");
+        String[] temp = input.split("\",|,\"|\",\"");
+
+        //allocate memory for correct amount of variables.
+        varName = new String[temp.length];
+        varType = new String[temp.length];
+        value = new String[temp.length];
+
+        //insert details into these arrays.
+        for(int count = 0; count < temp.length ; count++)
+        {
+            if(temp[count].length()-temp[count].replace("\"", "").length() == 2 && temp[count].charAt(0) == '\"')
+            {
+                varType[count] = "int";
+            }
+            else
+            {
+                varType[count] = "String";
+            }
+
+            temp[count] = temp[count].replace("\"", "");
+            varName[count] = temp[count].split(":")[0];
+
+            value[count] = temp[count].split(":")[1];
+        }
+    }
+
+    //used to construct a JSON String which is used for multi variables.
+    public static String constructJSONString(String[] varNames, String[] varTypes, String[] values)
+    {
+        String temp = "{";
+        for(int count = 0; count < varNames.length-1 ; count++)
+        {
+            temp+= "\"" + varNames[count] + "\":";
+            if(varTypes[count].equalsIgnoreCase("String"))
+            {
+                temp+= "\"" + values[count] + "\",";
+            }
+            else
+            {
+                temp+= values[count] + ",";
+            }
+        }
+        temp+= temp+= "\"" + varNames[varNames.length] + "\":";
+        if(varTypes[varNames.length].equalsIgnoreCase("String"))
+        {
+            temp+= "\"" + values[varNames.length] + "\"";
+        }
+        else
+        {
+            temp+= values[varNames.length];
+        }
+        temp+= "}";
+        return temp;
+    }
+
+    //returns value based on string variable created
+    public String getValueFromVar(String in)
+    {
+        //iterate through the list and return value once found
+        for(int count = 0; count < varName.length ; count++)
+        {
+            if(in.equalsIgnoreCase(varName[count]))
+            {
+                return value[count];
+            }
+        }
+        return null;
+    }
+
+    //returns the whole JSON file values in order of appearance in one string for SQL use
+    public String getWholeValue()
+    {
+        String temp = "";
+        for(int c = 0 ; c < value.length-1 ; c++)
+        {
+            if(varType[c].equalsIgnoreCase("String"))
+            {
+                temp += "\"" + value[c] + "\",";
+            }
+            else
+            {
+                temp += "" + value[c] + ",";
+            }
+        }
+        if(varType[value.length-1].equalsIgnoreCase("String"))
+        {
+            temp+= "\""+value[value.length-1]+"\"";
+        }
+        else
+        {
+            temp+= value[value.length-1];
+        }
+
+        return temp;
+    }
+}
--- WitsCABS Code/WitsCabs/app/src/main/java/com/example/user01/witscabs/signUpPage.java
+++ WitsCABS Code/WitsCabs/app/src/main/java/com/example/user01/witscabs/signUpPage.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
-import org.apache.http.NameValuePair;
+/*import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.message.BasicNameValuePair;*/
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class signUpPage extends Activity {
-    String name, lastName, phone, homeNumber, address, area, carReg, carMake, carColour;
+    String Home_Area, First_Name, Last_Name, Phone_Number, Home_StreetName, Car_Reg, Home_Number, Car_Make, Car_Colour;
     EditText firstNameET, lastNameET, phoneET, homePhoneET, addressET, carRegET, carMakeET, carColourET;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,26 +31,40 @@ public class signUpPage extends Activity {
     }
     public void getSignUpDetails(final View v){
         firstNameET = (EditText)findViewById(R.id.firstNameE);
-        name = firstNameET.getText().toString();
+        First_Name = firstNameET.getText().toString();
         lastNameET = (EditText)findViewById(R.id.lastNameE);
-        lastName = lastNameET.getText().toString();
+        Last_Name = lastNameET.getText().toString();
         phoneET = (EditText)findViewById(R.id.phoneE);
-        phone = phoneET.getText().toString();
+        Phone_Number = phoneET.getText().toString();
         homePhoneET = (EditText)findViewById(R.id.homePhoneE);
-        homeNumber = homePhoneET.getText().toString();
+        Home_Number = homePhoneET.getText().toString();
         addressET = (EditText)findViewById(R.id.addressE);
-        address = addressET.getText().toString();
+        Home_StreetName = addressET.getText().toString();
         carRegET = (EditText)findViewById(R.id.carRegE);
-        carReg = carRegET.getText().toString();
+        Car_Reg = carRegET.getText().toString();
         carMakeET = (EditText)findViewById(R.id.carMakeE);
-        carMake = carMakeET.getText().toString();
+        Car_Make = carMakeET.getText().toString();
         carColourET = (EditText)findViewById(R.id.carColourE);
-        carColour = carColourET.getText().toString();
+        Car_Colour = carColourET.getText().toString();
 
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://<ip address>:3000");
+        String[] varNames = {"First_Name", "Last_Name", "Phone_Number", "Home_Number", "Home_StreetName", "Car_Reg", "Car_Make", "Car_Colour", "Home_Area"};
+        String[] varTypes = {"String","String","String","int","String","String","String","String","String"};
+        String[] varActualVariables = new String[]{First_Name, Last_Name, Phone_Number, Home_Number, Home_StreetName, Car_Reg, Car_Make, Car_Colour, Home_Area};
 
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+        String json = JSON_Handler.constructJSONString(varNames, varTypes, varActualVariables);
+
+
+        AsyncHandler asyncHandler=new AsyncHandler() {
+            @Override
+            public void handleResponse(String response) {
+                if(response.equals("OK"))
+                    startActivity(new Intent(getApplicationContext(),loginPage.class));
+                else
+                    Toast.makeText(getApplicationContext(),"Signup failed",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        /*    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
             nameValuePairs.add(new BasicNameValuePair("firstName", name));
             nameValuePairs.add(new BasicNameValuePair("lastName", lastName));
             nameValuePairs.add(new BasicNameValuePair("phone", phone));
@@ -59,16 +73,7 @@ public class signUpPage extends Activity {
             nameValuePairs.add(new BasicNameValuePair("carReg", carReg));
             nameValuePairs.add(new BasicNameValuePair("carMake", carMake));
             nameValuePairs.add(new BasicNameValuePair("carColour", carColour));
-            AsyncHttpPost asyncHttpPost = new AsyncHttpPost(nameValuePairs, new AsyncHandler() {
-                @Override
-                public void handleResponse(String response) {
-                    response=response.trim();
-                    if(response.equals("success")){
-                        Toast.makeText(getApplicationContext(),"Registered",Toast.LENGTH_SHORT).show();
-                    }else Toast.makeText(getApplicationContext(),"Not Registered",Toast.LENGTH_SHORT).show();
-                }
-            });
-            asyncHttpPost.execute("http://");
+            asyncHttpPost.execute("http://10.30.40.41");
 
 /*        Toast.makeText(signUpPage.this,"before serrver",Toast.LENGTH_LONG);
         Socket client; String feedback;
