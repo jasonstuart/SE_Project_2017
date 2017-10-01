ffc2796c20219d96152397bf12eabc9b312123f8
Amine Boukrout <886515@students.wits.ac.za>
2017-10-01 16:04:03 +0200

    Final commit

---
+++ WitsCABS Code/WitsCabs/app/src/main/java/Modules/Distance.java
@@ -0,0 +1,11 @@
+package Modules;
+
+public class Distance {
+    public String text;
+    public int value;
+
+    public Distance(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
